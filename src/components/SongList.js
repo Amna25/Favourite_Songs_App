1aa7c6da0f55280abf4fd205e122b1f58668e094
Amna <amna.bashir2@gmail.com>
2022-01-11 11:51:15 +0000

    static for song

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Song from "./Song";
+
 
 const SongList = () => {
     return(
+        <>
         <h2>I am song list</h2>
+        <Song/>
+        </>
     )
 }
 
