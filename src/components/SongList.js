ddcf146bf1dd7f726397af40db84a624e3d82ee5
Amna <amna.bashir2@gmail.com>
2022-01-11 11:47:58 +0000

    static for soglist

---
+++ src/components/SongList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SongList = () => {
+    return(
+        <h2>I am song list</h2>
+    )
+}
+
+export default SongList;
--- src/containers/SongBox.js
+++ src/containers/SongBox.js
@@ -1,8 +1,12 @@
 import React from "react";
+import SongList from "../components/SongList";
 
 const SongBox = () => {
     return(
+        <>
         <h1> I am song box</h1>
+        <SongList/>
+        </>
 
     );
 }
